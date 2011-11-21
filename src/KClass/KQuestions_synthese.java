d0f4553ccf24e9df4d3b35c19be950239b09c04f
G4llic4 <g4llic4@gmail.com>
2011-11-21 10:29:47 +0100

    Correctoion des KClass suite au changement de bdd

--- src/KClass/KQuestions_synthese.java
+++ src/KClass/KQuestions_synthese.java
@@ -1,82 +1,60 @@
-
 package KClass;
 
 import net.ko.kobject.KListObject;
 import net.ko.kobject.KObject;
-
 /**
- * Classe KQuestions_synthese
- */
-@SuppressWarnings("serial")
-public class KQuestions_synthese extends KObject
-{
-	private String					LIBELLE_QUESTION_SYNTHESE;
-	private KListObject<KReponse>	reponses;
-	private long					idSYNTHESE;
-	private KSynthese				synthese;
-	
-	public KQuestions_synthese()
-	{
-		super();
-		keyFields = "id";
-		tableName = "questions_synthese";
-		belongsTo(KSynthese.class);
-		hasMany(KReponse.class);
-		
-	}
-	
-	public String getLIBELLE_QUESTION_SYNTHESE()
-	{
-		// return the value of LIBELLE_QUESTION_SYNTHESE
-		return this.LIBELLE_QUESTION_SYNTHESE;
-	}
-	
-	public KListObject<KReponse> getReponses()
-	{
-		// return the value of reponses
-		return this.reponses;
-	}
-	
-	public long getIdSYNTHESE()
-	{
-		// return the value of idSYNTHESE
-		return this.idSYNTHESE;
-	}
-	
-	public KSynthese getSynthese()
-	{
-		// return the value of synthese
-		return this.synthese;
-	}
-	
-	public void setLIBELLE_QUESTION_SYNTHESE(String aLIBELLE_QUESTION_SYNTHESE)
-	{
-		// set the value of LIBELLE_QUESTION_SYNTHESE
-		this.LIBELLE_QUESTION_SYNTHESE = aLIBELLE_QUESTION_SYNTHESE;
-	}
-	
-	public void setReponses(KListObject<KReponse> aReponses)
-	{
-		// set the value of reponses
-		this.reponses = aReponses;
-	}
-	
-	public void setIdSYNTHESE(long aIdSYNTHESE)
-	{
-		// set the value of idSYNTHESE
-		this.idSYNTHESE = aIdSYNTHESE;
-	}
-	
-	public void setSynthese(KSynthese aSynthese)
-	{
-		// set the value of synthese
-		this.synthese = aSynthese;
-	}
-	
-	@Override
-	public String toString()
-	{
-		return " [LIBELLE_QUESTION_SYNTHESE] = " + LIBELLE_QUESTION_SYNTHESE + " [reponses] = " + reponses
-				+ " [idSYNTHESE] = " + idSYNTHESE + " [synthese] = " + synthese;
-	}
+* Classe KQuestions_synthese
+*/
+public class KQuestions_synthese extends KObject {
+private String LIBELLE_QUESTION_SYNTHESE;
+private int idTHEME_SYNTHESE;
+private KListObject<KReponse> reponses;
+private KThemes_synthese themes_synthese;
+public KQuestions_synthese() {
+	super();
+	keyFields="id";
+	tableName="questions_synthese";
+	//belongsTo(KThemes_synthese.class);hasMany(KReponse.class);
+
+}
+public String getLIBELLE_QUESTION_SYNTHESE(){
+	//return the value of LIBELLE_QUESTION_SYNTHESE
+	return this.LIBELLE_QUESTION_SYNTHESE;
+}
+
+public int getIdTHEME_SYNTHESE(){
+	//return the value of idTHEME_SYNTHESE
+	return this.idTHEME_SYNTHESE;
+}
+
+public KListObject<KReponse> getReponses(){
+	//return the value of reponses
+	return this.reponses;
+}
+
+public KThemes_synthese getThemes_synthese(){
+	//return the value of themes_synthese
+	return this.themes_synthese;
+}
+
+public void setLIBELLE_QUESTION_SYNTHESE(String aLIBELLE_QUESTION_SYNTHESE){
+	//set the value of LIBELLE_QUESTION_SYNTHESE
+	this.LIBELLE_QUESTION_SYNTHESE=aLIBELLE_QUESTION_SYNTHESE;
+}
+public void setIdTHEME_SYNTHESE(int aIdTHEME_SYNTHESE){
+	//set the value of idTHEME_SYNTHESE
+	this.idTHEME_SYNTHESE=aIdTHEME_SYNTHESE;
+}
+public void setReponses(KListObject<KReponse> aReponses){
+	//set the value of reponses
+	this.reponses=aReponses;
+}
+public void setThemes_synthese(KThemes_synthese aThemes_synthese){
+	//set the value of themes_synthese
+	this.themes_synthese=aThemes_synthese;
+}
+@Override
+public String toString() {
+	return " [LIBELLE_QUESTION_SYNTHESE] = " + LIBELLE_QUESTION_SYNTHESE+" [idTHEME_SYNTHESE] = " + idTHEME_SYNTHESE+" [reponses] = " + reponses+" [themes_synthese] = " + themes_synthese;
+}
 }
--- src/KClass/KThemes_synthese.java
+++ src/KClass/KThemes_synthese.java
@@ -5,33 +5,26 @@ import net.ko.kobject.KObject;
 /**
 * Classe KThemes_synthese
 */
-@SuppressWarnings("serial")
 public class KThemes_synthese extends KObject {
-private long ID_SYNTHESE;
 private String LIBELLE_THEMES_SYNTHESE;
-private int ID;
+private long idSYNTHESE;
 private KListObject<KQuestions_synthese> questions_syntheses;
 private KSynthese synthese;
 public KThemes_synthese() {
 	super();
-	keyFields="ID";
+	keyFields="id";
 	tableName="themes_synthese";
 	//hasMany(KQuestions_synthese.class);belongsTo(KSynthese.class);
 
 }
-public long getID_SYNTHESE(){
-	//return the value of ID_SYNTHESE
-	return this.ID_SYNTHESE;
-}
-
 public String getLIBELLE_THEMES_SYNTHESE(){
 	//return the value of LIBELLE_THEMES_SYNTHESE
 	return this.LIBELLE_THEMES_SYNTHESE;
 }
 
-public int getID(){
-	//return the value of ID
-	return this.ID;
+public long getIdSYNTHESE(){
+	//return the value of idSYNTHESE
+	return this.idSYNTHESE;
 }
 
 public KListObject<KQuestions_synthese> getQuestions_syntheses(){
@@ -44,17 +37,13 @@ public KSynthese getSynthese(){
 	return this.synthese;
 }
 
-public void setID_SYNTHESE(long aID_SYNTHESE){
-	//set the value of ID_SYNTHESE
-	this.ID_SYNTHESE=aID_SYNTHESE;
-}
 public void setLIBELLE_THEMES_SYNTHESE(String aLIBELLE_THEMES_SYNTHESE){
 	//set the value of LIBELLE_THEMES_SYNTHESE
 	this.LIBELLE_THEMES_SYNTHESE=aLIBELLE_THEMES_SYNTHESE;
 }
-public void setID(int aID){
-	//set the value of ID
-	this.ID=aID;
+public void setIdSYNTHESE(long aIdSYNTHESE){
+	//set the value of idSYNTHESE
+	this.idSYNTHESE=aIdSYNTHESE;
 }
 public void setQuestions_syntheses(KListObject<KQuestions_synthese> aQuestions_syntheses){
 	//set the value of questions_syntheses
@@ -66,6 +55,6 @@ public void setSynthese(KSynthese aSynthese){
 }
 @Override
 public String toString() {
-	return " [ID_SYNTHESE] = " + ID_SYNTHESE+" [LIBELLE_THEMES_SYNTHESE] = " + LIBELLE_THEMES_SYNTHESE+" [ID] = " + ID+" [questions_syntheses] = " + questions_syntheses+" [synthese] = " + synthese;
+	return " [LIBELLE_THEMES_SYNTHESE] = " + LIBELLE_THEMES_SYNTHESE+" [idSYNTHESE] = " + idSYNTHESE+" [questions_syntheses] = " + questions_syntheses+" [synthese] = " + synthese;
 }
 }
